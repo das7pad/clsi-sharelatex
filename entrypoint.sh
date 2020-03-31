688e41637ffdb92098e6ff6528a0f3b59545a497
Jakob Ackermann <das7pad@outlook.com>
2020-03-31 21:17:06 +0200

    [misc] ci: initialize the source directories for bind mounts
    Signed-off-by: Jakob Ackermann <das7pad@outlook.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,11 @@ DOCKER_GROUP=$(stat -c '%g' /var/run/docker.sock)
 groupadd --non-unique --gid ${DOCKER_GROUP} dockeronhost
 usermod -aG dockeronhost node
 
+# compat
+chown node:node /app/cache
+chown node:node /app/compiles
+chown node:node /app/db
+
 mkdir -p /app/test/acceptance/fixtures/tmp/
 chown node:node /app/test/acceptance/fixtures/tmp/
 
