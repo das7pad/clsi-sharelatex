4873e84314b15c0d73aad19601881a614b96a747
Jakob Ackermann <das7pad@outlook.com>
2020-08-17 01:13:12 +0200

    [misc] fix image conversion

--- install_deps.sh
+++ install_deps.sh
@@ -8,3 +8,7 @@ apt-get update && \
       qpdf \
   \
 && rm -rf /var/lib/apt/lists/*
+
+# https://stackoverflow.com/questions/52998331/imagemagick-security-policy-pdf-blocking-conversion#comment110879511_59193253
+# License: CC BY-SA 4.0
+sed -i '/disable ghostscript format types/,+6d' /etc/ImageMagick-6/policy.xml
